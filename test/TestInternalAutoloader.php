af048b8736ffb1f1ba0a6544ec1652d22973d0d2
kutiku <kutiku@521bd094-c7e3-4c17-b425-2dfe5f6de0f6>
2010-05-19 22:19:04 +0000

    TestInternalAutoloader follows the pear coding standards.

--- test/TestInternalAutoloader.php
+++ test/TestInternalAutoloader.php
@@ -1,32 +1,46 @@
 <?php
-#########################################################################
-# Copyright (C) 2010  Markus Malkusch <markus@malkusch.de>              #
-#                                                                       #
-# This program is free software: you can redistribute it and/or modify  #
-# it under the terms of the GNU General Public License as published by  #
-# the Free Software Foundation, either version 3 of the License, or     #
-# (at your option) any later version.                                   #
-#                                                                       #
-# This program is distributed in the hope that it will be useful,       #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
-# GNU General Public License for more details.                          #
-#                                                                       #
-# You should have received a copy of the GNU General Public License     #
-# along with this program.                                              #
-# If not, see <http://php-autoloader.malkusch.de/en/license/>.          #
-#########################################################################
 
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
+/**
+ * This file defines the test cases for the class InternalAutoloader.
+ *
+ * PHP version 5
+ *
+ * LICENSE: This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.
+ * If not, see <http://php-autoloader.malkusch.de/en/license/>.
+ *
+ * @category  Autoloader
+ * @package   Test
+ * @author    Markus Malkusch <markus@malkusch.de>
+ * @copyright 2009 - 2010 Markus Malkusch
+ * @license   http://php-autoloader.malkusch.de/en/license/ GPL 3
+ * @version   SVN: $Id$
+ * @link      http://php-autoloader.malkusch.de/en/
+ * @see       InternalAutoloader
+ */
+
+/**
+ * The Autoloader is used for class loading.
+ */
 require_once dirname(__FILE__) . "/../Autoloader.php";
 
 
 /**
  * InternalAutoloader test cases.
- * 
- * Copyright (C) 2010  Markus Malkusch <markus@malkusch.de>
  *
- * This program is free software; you can redistribute it and/or modify
+ * LICENSE: This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
@@ -40,87 +54,161 @@ require_once dirname(__FILE__) . "/../Autoloader.php";
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * @package Autoloader
- * @subpackage test
- * @author Markus Malkusch <markus@malkusch.de>
- * @copyright Copyright (C) 2010 Markus Malkusch
+ * @category  Autoloader
+ * @package   Test
+ * @author    Markus Malkusch <markus@malkusch.de>
+ * @copyright 2009 - 2010 Markus Malkusch
+ * @license   http://php-autoloader.malkusch.de/en/license/ GPL 3
+ * @version   Release: 1.8
+ * @link      http://php-autoloader.malkusch.de/en/
  */
-class TestInternalAutoloader extends PHPUnit_Framework_TestCase {
+class TestInternalAutoloader extends PHPUnit_Framework_TestCase
+{
 
-	
-	/**
-     * @param String $class
-     * @param String $path
+    /**
+     * testAutoload() asserts that the class $class is loadable after
+     * registration of its class definition $path.
+     *
+     * @param String $class The class which is autoloaded
+     * @param String $path  The class defintion
+     * 
      * @dataProvider provideTestAutoload
+     * @return void
+     * @see InternalAutoloader::registerClass()
      */
-    public function testAutoload($class, $path) {
+    public function testAutoload($class, $path)
+    {
         $autoloaderTestHelper = new AutoloaderTestHelper($this);
-        
+
         Autoloader::removeAll();
-        
+
         $autoloaderTestHelper->assertNotLoadable($class);
         InternalAutoloader::getInstance()->registerClass($class, $path);
         $autoloaderTestHelper->assertLoadable($class);
     }
-    
-	
-	public function testGetInstance() {
-		$this->assertTrue(InternalAutoloader::getInstance() instanceof InternalAutoloader);
-		$this->assertTrue(InternalAutoloader::getInstance()->isRegistered());
-	}
-	
-	
-	public function testSingleton() {
-		$this->assertEquals(1, count(InternalAutoloader::getRegisteredAutoloaders()));
-	}
-	
-	
-	public function testRemoveAll() {
-		InternalAutoloader::removeAll();
-		$this->assertRemoved();
-	}
-	
-	
-	public function testRemove() {
-		InternalAutoloader::getInstance()->remove();
-		$this->assertRemoved();
-	}
-	
 
-	/**
-	 * @return array
-	 */
-	public function provideTestAutoload() {
-		$autoloaderTestHelper = new AutoloaderTestHelper($this);
-		$return               = array();
-		
-		$class    = $autoloaderTestHelper->makeClass("ClassA", 'testInternal');
-		$return[] = array(
-            $class,
-            $autoloaderTestHelper->getGeneratedClassPath($class)
+    /**
+     * testGetInstance() asserts that InternalAutoloader::getInstance() returns
+     * an instance of InternalAutoloader and that this instance is registered
+     * in the autoloader stack.
+     *
+     * @return void
+     * @see InternalAutoloader::getInstance()
+     */
+    public function testGetInstance()
+    {
+        $this->assertTrue(
+            InternalAutoloader::getInstance() instanceof InternalAutoloader
         );
-		$class    = $autoloaderTestHelper->makeClass("ClassA2", 'testInternal');
-		$return[] = array(
-            $class,
-            $autoloaderTestHelper->getGeneratedClassPath($class)
+        $this->assertTrue(
+            InternalAutoloader::getInstance()->isRegistered()
         );
-		$class    = $autoloaderTestHelper->makeClass("ClassB", 'testInternal/sub');
-		$return[] = array(
-            $class,
-            $autoloaderTestHelper->getGeneratedClassPath($class)
-        );
-		
-		return $return;
-	}
-
-    
-    private function assertRemoved() {
-        $this->assertFalse(InternalAutoloader::getInstance()->isRegistered());
-        $this->assertEquals(0, count(InternalAutoloader::getRegisteredAutoloaders()));
-        
-        InternalAutoloader::getInstance()->register();
-        $this->assertTrue(InternalAutoloader::getInstance()->isRegistered());
     }
-    
-    
+
+    /**
+     * testSingleton() asserts that there exists only one registered instance
+     * of InternalAutoloader.
+     *
+     * @return void
+     */
+    public function testSingleton()
+    {
+        $this->assertEquals(
+            1,
+            count(InternalAutoloader::getRegisteredAutoloaders())
+        );
+    }
+
+    /**
+     * testRemoveAll() asserts that after calling
+     * InternalAutoloader::removeAll() no instance of InternalAutoloader is
+     * registered.
+     *
+     * @return void
+     * @see InternalAutoloader::removeAll()
+     */
+    public function testRemoveAll()
+    {
+        InternalAutoloader::removeAll();
+        $this->_assertRemoved();
+    }
+
+    /**
+     * testRemove() asserts that after calling
+     * InternalAutoloader::getInstance()->remove() the instance (which is the
+     * only existing one) is removed from the autoloader stack.
+     *
+     * @return void
+     * @see InternalAutoloader::remove()
+     */
+    public function testRemove()
+    {
+        InternalAutoloader::getInstance()->remove();
+        $this->_assertRemoved();
+    }
+
+    /**
+     * provideTestAutoload() returns test cases for testAutoload().
+     *
+     * A test case is class name and a path to the definition of this
+     * class name.
+     *
+     * @see testAutoload()
+     * @return array
+     */
+    public function provideTestAutoload()
+    {
+        $autoloaderTestHelper = new AutoloaderTestHelper($this);
+        $return               = array();
+
+        $class    = $autoloaderTestHelper->makeClass(
+            "ClassA",
+            'testInternal'
+        );
+        $return[] = array(
+            $class,
+            $autoloaderTestHelper->getGeneratedClassPath($class)
+        );
+        $class    = $autoloaderTestHelper->makeClass(
+            "ClassA2",
+            'testInternal'
+        );
+        $return[] = array(
+            $class,
+            $autoloaderTestHelper->getGeneratedClassPath($class)
+        );
+        $class    = $autoloaderTestHelper->makeClass(
+            "ClassB",
+            'testInternal/sub'
+        );
+        $return[] = array(
+            $class,
+            $autoloaderTestHelper->getGeneratedClassPath($class)
+        );
+
+        return $return;
+    }
+
+    /**
+     * _assertRemoved() asserts that no instance of InternalAutoloader is
+     * registered. It also asserts that registration would still work.
+     *
+     * @return void
+     */
+    private function _assertRemoved()
+    {
+        $this->assertFalse(
+            InternalAutoloader::getInstance()->isRegistered()
+        );
+        $this->assertEquals(
+            0,
+            count(InternalAutoloader::getRegisteredAutoloaders())
+        );
+
+        InternalAutoloader::getInstance()->register();
+        $this->assertTrue(
+            InternalAutoloader::getInstance()->isRegistered()
+        );
+    }
+
 }

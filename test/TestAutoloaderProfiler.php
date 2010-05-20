3a64e13a566ea66b5eeb194f09543feee396c974
kutiku <kutiku@521bd094-c7e3-4c17-b425-2dfe5f6de0f6>
2010-05-20 10:26:47 +0000

    test/TestAutoloaderProfiler.php follows the pear coding standards.

--- test/TestAutoloaderProfiler.php
+++ test/TestAutoloaderProfiler.php
@@ -1,33 +1,50 @@
 <?php
-#########################################################################
-# Copyright (C) 2010  Markus Malkusch <markus@malkusch.de>              #
-#                                                                       #
-# This program is free software: you can redistribute it and/or modify  #
-# it under the terms of the GNU General Public License as published by  #
-# the Free Software Foundation, either version 3 of the License, or     #
-# (at your option) any later version.                                   #
-#                                                                       #
-# This program is distributed in the hope that it will be useful,       #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
-# GNU General Public License for more details.                          #
-#                                                                       #
-# You should have received a copy of the GNU General Public License     #
-# along with this program.                                              #
-# If not, see <http://php-autoloader.malkusch.de/en/license/>.          #
-#########################################################################
 
-
-require_once dirname(__FILE__) . "/../Autoloader.php";
-require_once dirname(__FILE__) . "/../classes/Autoloader_Profiler.php";
-
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
 /**
- * Autoloader_Profiler test cases.
- * 
- * Copyright (C) 2010  Markus Malkusch <markus@malkusch.de>
+ * This file defines the test cases for Autoloader_Profiler.
  *
- * This program is free software; you can redistribute it and/or modify
+ * PHP version 5
+ *
+ * LICENSE: This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.
+ * If not, see <http://php-autoloader.malkusch.de/en/license/>.
+ *
+ * @category  Autoloader
+ * @package   Test
+ * @author    Markus Malkusch <markus@malkusch.de>
+ * @copyright 2009 - 2010 Markus Malkusch
+ * @license   http://php-autoloader.malkusch.de/en/license/ GPL 3
+ * @version   SVN: $Id$
+ * @link      http://php-autoloader.malkusch.de/en/
+ * @see       Autoloader_Profiler
+ */
+
+/**
+ * The Autoloader is used for class loading.
+ */
+require_once dirname(__FILE__) . "/../Autoloader.php";
+
+/**
+ * Autoloader_Profiler is not not registered in the InternalAutoloader.
+ */
+require_once dirname(__FILE__) . "/../classes/Autoloader_Profiler.php";
+
+/**
+ * Autoloader_Profiler test cases
+ *
+ * LICENSE: This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
@@ -41,51 +58,64 @@ require_once dirname(__FILE__) . "/../classes/Autoloader_Profiler.php";
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * @package Autoloader
- * @subpackage test
- * @author Markus Malkusch <markus@malkusch.de>
- * @copyright Copyright (C) 2010 Markus Malkusch
+ * @category  Autoloader
+ * @package   Test
+ * @author    Markus Malkusch <markus@malkusch.de>
+ * @copyright 2009 - 2010 Markus Malkusch
+ * @license   http://php-autoloader.malkusch.de/en/license/ GPL 3
+ * @version   Release: 1.8
+ * @link      http://php-autoloader.malkusch.de/en/
  */
-class TestAutoloaderProfiler extends PHPUnit_Framework_TestCase {
-	
-	
-	private
-	/**
-	 * @var AutoloaderTestHelper
-	 */
-	$autoloaderTestHelper;
-	
-	
-    public function setUp() {
-		$this->autoloaderTestHelper = new AutoloaderTestHelper($this);
+class TestAutoloaderProfiler extends PHPUnit_Framework_TestCase
+{
+
+    private
+    /**
+     * @var AutoloaderTestHelper
+     */
+    $_autoloaderTestHelper;
+
+
+    /**
+     * setUp() removes all Autoloaders from the stack and initializes
+     * the AutoloaderTestHelper $_autoloaderTestHelper.
+     *
+     * @see $_autoloaderTestHelper
+     * @return void
+     */
+    public function setUp()
+    {
+        $this->_autoloaderTestHelper = new AutoloaderTestHelper($this);
         Autoloader::removeAll();
-	}
-	
-	
-	public function tearDown() {
-	}
+    }
 
-
-    public function testUseIndexInMultiALEnvironment() {
-        $classA = $this->autoloaderTestHelper->makeClass('A', 'a');
-        $classB = $this->autoloaderTestHelper->makeClass('B', 'b');
+    /**
+     * testUseIndexInMultiAutoLoaderEnvironment() asserts that in an environment
+     * with several registered instances of Autoloader, every Autoloader uses its
+     * index first before they start iterating through their class paths.
+     *
+     * @return void
+     */
+    public function testUseIndexInMultiAutoLoaderEnvironment()
+    {
+        $classA = $this->_autoloaderTestHelper->makeClass('A', 'a');
+        $classB = $this->_autoloaderTestHelper->makeClass('B', 'b');
 
         $alA = new Autoloader_Profiler(
-            dirname($this->autoloaderTestHelper->getGeneratedClassPath($classA)));
+            dirname($this->_autoloaderTestHelper->getGeneratedClassPath($classA)));
         $alA->register();
         $alA->addClassToIndex($classA);
 
         $alB = new Autoloader_Profiler(
-            dirname($this->autoloaderTestHelper->getGeneratedClassPath($classB)));
+            dirname($this->_autoloaderTestHelper->getGeneratedClassPath($classB)));
         $alB->register();
         $alB->addClassToIndex($classB);
 
-        $this->autoloaderTestHelper->assertLoadable($classA);
-        $this->autoloaderTestHelper->assertLoadable($classB);
+        $this->_autoloaderTestHelper->assertLoadable($classA);
+        $this->_autoloaderTestHelper->assertLoadable($classB);
 
         $this->assertEquals(array(), $alA->getSearchedClasses());
         $this->assertEquals(array(), $alB->getSearchedClasses());
     }
-	
-	
+
 }
